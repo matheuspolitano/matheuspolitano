99ae65cc94a35332bf174ef04f8d8fb9b6ab333d
matheus politano <7602799-matheuspolitano@users.noreply.gitlab.com>
2022-06-01 21:39:23 -0300

    First commit - base project

---
+++ bookstore/ebook/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.CharField(max_length=100, default='John Doe')
+    isbn = models.CharField(max_length=13)
+    pages = models.IntegerField()
+    price = models.IntegerField()
+    quantity = models.IntegerField()
+    description = models.TextField()
+    status = models.BooleanField()
+    date_created = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_created']
+
+    def __str__(self):
+        return self.title
